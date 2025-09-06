fa7a9cc5552c6bb0db9b9710bbddbd9d5c3293df
Vinicius Bavosa <viniciusbavosa@outlook.com>
2025-09-05 22:51:27 -0300

    feat: implement animal validation and input handling

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,7 +1,60 @@
+const animalsArr = [{
+  nome: 'REX',
+  raça: 'CAO',
+  brinquedos: ['RATO, BOLA']
+}, {
+  nome: 'MIMI',
+  raça: 'GATO',
+  brinquedos: ['BOLA', 'LASER']
+}, {
+  nome: 'FOFO',
+  raça: 'GATO',
+  brinquedos: ['BOLA, RATO, LASER']
+}, {
+  nome: 'ZERO',
+  raça: 'GATO',
+  brinquedos: ['RATO, BOLA']
+}, {
+  nome: 'BOLA',
+  raça: 'CAO',
+  brinquedos: ['CAIXA, NOVELO']
+}, {
+  nome: 'BEBE',
+  raça: 'CAO',
+  brinquedos: ['LASER, RATO, BOLA']
+}, {
+  nome: 'LOCO',
+  raça: 'JABUTI',
+  brinquedos: ['SKATE, RATO']
+}];
+
+
+const InvalidAnimalException = {
+  lista: null,
+  erro: 'Animal inválido'
+}
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    const firstPersonInput = brinquedosPessoa1.toUpperCase().trim();
+    const secondPersonInput = brinquedosPessoa2.toUpperCase().trim();
+    const chosenAnimals = ordemAnimais.toUpperCase().trim();
+
+    if (!this.isValidAnimal(chosenAnimals)) return InvalidAnimalException;
+
+  }
+
+  isValidAnimal(animal) {
+    const animals = animal.split(',');
+    let result;
+
+    animals.forEach((animalInput, _) => {
+      result = animalsArr.some((animalData, _) => animalData.nome === animalInput)
+    })
+
+    return result;
   }
 }
+
 
 export { AbrigoAnimais as AbrigoAnimais };
